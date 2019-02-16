489c689a413103013fa604f5b2da86450618c4e1
Ken <kenpm0x0a@plusuno.jp>
2019-02-16 13:09:53 +0900

    --

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+var config = require("./webpack.config");
 
+const port = 3007;
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: false,
   historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
+}).listen(port, "localhost", function(err, result) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Listening at localhost:3000');
+  console.log(`Listening at localhost:${port}`);
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,25 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
-  devtool: 'eval',
-  mode: 'development',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    './src/index'
-  ],
+  devtool: "eval",
+  mode: "development",
+  entry: ["webpack-dev-server/client?http://localhost:3007", "./src/index"],
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/static/"
   },
   resolve: {
-    extensions: ['.js', '.ts', '.tsx']
+    extensions: [".ts", ".tsx", ".js"]
   },
   module: {
-    rules: [{
-      test: /\.tsx?$/,
-      loader: "awesome-typescript-loader",
-      include: path.join(__dirname, 'src')
-    }]
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+        include: path.join(__dirname, "src")
+      }
+    ]
   }
 };
